b1ff901f92d0b3318fd956fb814ad3124b82e2a9
Naoki Sawada <naoki.sawada@nextremer.com>
2018-08-18 22:13:48 +0900

    Added temperature and humidity accessories

--- homebot/src/controller_server.py
+++ homebot/src/controller_server.py
@@ -7,6 +7,11 @@ from controller import *
 
 ctl = Control()
 
+class TmphumHandler(tornado.web.RequestHandler):
+    def get(self):
+        res = ctl.get_tmphum()
+        self.write(res)
+
 class MyHandler(tornado.web.RequestHandler):
     def post(self):
         data = json.loads(self.request.body.decode('utf-8'))
@@ -15,7 +20,10 @@ class MyHandler(tornado.web.RequestHandler):
         self.write(res)
 
 if __name__ == '__main__':
-    app = tornado.web.Application([ tornado.web.url(r'/', MyHandler) ])
+    app = tornado.web.Application([
+        tornado.web.url(r'/', MyHandler),
+        tornado.web.url(r'/tmphum', TmphumHandler)
+    ])
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(8888)
     print('Starting server on port 8888')
